0884968248b0dd211db340a32b0811adb53f916a
VeronicaVB <verisb@gmail.com>
2024-07-28 08:59:02 +1000

    Task 3 completed

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests  # Import the requests library to handle HTTP requests
+import json
 
 
 def emotion_detector(text_to_analyse):
@@ -10,6 +11,22 @@ def emotion_detector(text_to_analyse):
     # text to be analyzed
     response = requests.post(url, json=myobj, headers=headers)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotions = (formatted_response['emotionPredictions'][0])
 
+    anger_score = emotions['emotion']['anger']
+    disgust_score = emotions['emotion']['disgust']
+    fear_score = emotions['emotion']['fear']
+    joy_score = emotions['emotion']['joy']
+    sadness_score = emotions['emotion']['sadness']
+    dominant_emotion = max([anger_score, disgust_score, fear_score, joy_score, sadness_score])
 
+    output = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    return output
